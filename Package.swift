a32a31e89a86d97f8f5e90336d2bf133189dc96a
Lightech <lightech@outlook.com>
2021-05-27 15:19:51 +0700

    Update package Clibgit2 URL

--- Package.swift
+++ Package.swift
@@ -14,11 +14,15 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "Clibgit2", url: "https://github.com/light-tech/LibGit2-On-iOS.git", .branch("master")),
+        .package(
+            name: "Clibgit2",
+            url: "https://github.com/light-tech/Clibgit2.git",
+            .branch("master")
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other target  s in this package, and on products in packages this package depends on.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftGit2",
             dependencies: ["Clibgit2"],

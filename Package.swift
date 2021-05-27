c2deee202e8e5d6f13fcb7f04108155faaaa0b8d
Lightech <lightech@outlook.com>
2021-05-26 20:04:20 -0700

    Declare Clibgit2 as a dependency package instead of as a binaryTarget

--- Package.swift
+++ Package.swift
@@ -14,20 +14,15 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "Clibgit2", url: "https://github.com/light-tech/LibGit2-On-iOS.git", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        // Targets can depend on other target  s in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftGit2",
             dependencies: ["Clibgit2"],
             path: "SwiftGit2"
-        ),
-        .binaryTarget(
-            name: "Clibgit2",
-            url: "https://github.com/light-tech/LibGit2-On-iOS/releases/download/v1.1.0/Clibgit2.xcframework.zip",
-            checksum: "f10e544f6180c23a4de9a965e6bc45ea3ee92b08ebbe9c7f8733446fb7c2d1db"
         ),
         .testTarget(
             name: "SwiftGit2Tests",
